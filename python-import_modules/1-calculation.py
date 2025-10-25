38b939a405a41bb642dc55f508c6112f810bdb17
David-Irihose <d.irihose@alustudent.com>
2025-10-25 05:06:03 -0700

    Fix Task 1: correct shebang, import, variables, and prints

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 import calculator_1
 
+add = calculator_1.add
+sub = calculator_1.sub
+mul = calculator_1.mul
+div = calculator_1.div
+
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
