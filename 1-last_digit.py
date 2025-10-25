a24e6361a14eb06c93fc4d7456b9ec7ba508c773
David-Irihose <d.irihose@alustudent.com>
2025-10-25 03:44:33 -0700

    Task 1: print last digit of a random number with description

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -2,11 +2,10 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = number % 10 if number >= 0 else -(-number % 10)
 
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print("{} is negative".format(number))
