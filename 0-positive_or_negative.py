e042102e8631b83958a728b381b53ab198ca1716
David <d.irihose@alustudent.com>
2025-10-30 14:29:47 +0200

    Update 0-positive_or_negative.py

--- 0-positive_or_negative.py
+++ 0-positive_or_negative.py
@@ -3,7 +3,9 @@ import random
 
 number = random.randint(-1000, 1000)
 
-if number > 0:
+if not isinstance(number, int):
+    print("wrong type")
+elif number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
