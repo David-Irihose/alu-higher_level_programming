bf4265d616cc0bc4a5fcb501eeb35bc9c22e8696
David-Irihose <d.irihose@alustudent.com>
2025-10-31 06:46:59 -0700

    Fix: delete_at edge cases

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,4 +2,8 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    return my_list[:idx] + my_list[idx+1:]
+    # Shift elements to remove the element at idx
+    for i in range(idx, len(my_list) - 1):
+        my_list[i] = my_list[i + 1]
+    my_list.pop()  # remove the last duplicate
+    return my_list
